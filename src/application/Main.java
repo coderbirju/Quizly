3c6ac904189fc1fae725e2f06f9b14fa83568362
Raksha Kagadalu <98128238+Raksha-Kagadalu@users.noreply.github.com>
2023-04-04 15:17:28 -0700

    first commit

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,24 +3,22 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	Button btOK = new Button("Hello World");
+	Scene scene = new Scene(btOK, 200, 250);
+	primaryStage.setTitle("MyJavaFX");
+	primaryStage.setScene(scene);
+	primaryStage.show();
 	}
 	
 	public static void main(String[] args) {
-		launch(args);
+		Application.launch(args);
 	}
+	
 }
