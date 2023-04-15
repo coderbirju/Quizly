2c4ec1e2d117a35d7e11f41e390343aee1bd7d8b
Raksha Kagadalu <98128238+Raksha-Kagadalu@users.noreply.github.com>
2023-04-15 17:58:12 -0400

    added search fields and controllers for create quiz, attendance and analytics pa

---
+++ src/application/controller/ClassAttendance_Controller.java
@@ -0,0 +1,32 @@
+package application.controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ClassAttendance_Controller {
+
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    private Button btnSearch;
+
+    @FXML
+    private TextField txtQuizCode;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    void resetSearch(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchQuiz(ActionEvent event) {
+
+    }
+
+}
---
+++ src/application/controller/QuizAnalytics_Controller.java
@@ -0,0 +1,29 @@
+package application.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class QuizAnalytics_Controller {
+
+    @FXML
+    private Button btnReset2;
+
+    @FXML
+    private Button btnSearch2;
+
+    @FXML
+    private TextField txtQuizCode2;
+
+    @FXML
+    void resetSearch(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchQuiz2(ActionEvent event) {
+
+    }
+
+}
