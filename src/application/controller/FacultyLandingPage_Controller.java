e06ad1d55633896e4a8ba90b7ff7ac8ad856840d
Raksha Kagadalu <98128238+Raksha-Kagadalu@users.noreply.github.com>
2023-04-15 17:41:21 -0400

    created new quiz fields and controller

--- src/application/controller/FacultyLandingPage_Controller.java
+++ src/application/controller/FacultyLandingPage_Controller.java
@@ -10,7 +10,6 @@ import javafx.event.ActionEvent;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
