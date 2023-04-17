74e0a252760d851b6032e46ae7d0c4a45c0a916c
Raksha Kagadalu <98128238+Raksha-Kagadalu@users.noreply.github.com>
2023-04-17 15:15:08 -0400

    added table instead of text area and validation for empty field

--- src/application/controller/ClassAttendance_Controller.java
+++ src/application/controller/ClassAttendance_Controller.java
@@ -1,24 +1,22 @@
 package application.controller;
 import application.model.Professor;
 import application.model.Quiz;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.List;
-
 public class ClassAttendance_Controller {
 
     @FXML
@@ -29,43 +27,68 @@ public class ClassAttendance_Controller {
 
     @FXML
     private TextField txtQuizCode;
-
-    @FXML
-    private TextArea txtResult;
     
     @FXML
     private Button btnDownload;
 
     @FXML
+    private TableColumn<String, String> colStudent;
+
+    @FXML
+    private TableView<String> tblStudentView;
+    
+    @FXML
     void resetSearch(ActionEvent event) {
     	txtQuizCode.setText("");
-    	txtResult.setText("");
+    	//txtResult.setText("");
+    	tblStudentView.getItems().clear();
+    	tblStudentView.refresh();
     }
 
     @FXML
     //new713
     void searchQuiz(ActionEvent event) {
     	String quizId = txtQuizCode.getText();
-    	System.out.println("quiz ID from user is"+quizId);
+    	
+    	if(quizId.isEmpty()) {
+    		 Alert alert = new Alert(Alert.AlertType.ERROR);
+    	        alert.setHeaderText("Error");
+    	        alert.setContentText("Quiz ID field cannot be empty");
+    	        alert.showAndWait();
+    	        return;
+    	}
+    	
+    	//System.out.println("quiz ID from user is"+quizId);
     	 Professor prof = new Professor();
     	 Quiz quiz = prof.fetchQuizById(quizId);
     	 List<String> studentList = prof.getQuizAttendance(quiz);
-    	 for (String s : studentList) {
-    		 txtResult.appendText(s + "\n");
-    		}
+//    	 for (String s : studentList) {
+//    		 txtResult.appendText(s + "\n");
+//    		}
+    	 
+    	 colStudent.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue()));
+    	// tblStudentView.getColumns().add(colStudent);
+    	 tblStudentView.setItems(FXCollections.observableArrayList(studentList));
    
   // txtResult.setText(prof.getQuizAttendance(quiz).toString());
     }
     
     @FXML
     void download(ActionEvent event) {
-    	
-
     	// Create an ArrayList of student name from response
-    	ArrayList<String> list = new ArrayList<>();
-    	list.add("Hello");
-    	list.add("World");
-    	list.add("!");
+    	String quizId = txtQuizCode.getText();
+    	if(quizId.isEmpty()) {
+   		 Alert alert = new Alert(Alert.AlertType.ERROR);
+   	        alert.setHeaderText("Error");
+   	        alert.setContentText("Quiz ID field cannot be empty");
+   	        alert.showAndWait();
+   	     return;
+   	}
+    	//System.out.println("quiz ID from user is"+quizId);
+    	 Professor prof = new Professor();
+    	 Quiz quiz = prof.fetchQuizById(quizId);
+    	 List<String> studentList = prof.getQuizAttendance(quiz);
+
 
     	Stage primaryStage = (Stage) btnDownload.getScene().getWindow();
         // prompt the user to select a file to save the names to
@@ -76,7 +99,7 @@ public class ClassAttendance_Controller {
         // if the user selected a file, write the names to the file
         if (file != null) {
             try (PrintWriter writer = new PrintWriter(file)) {
-                for (String names : list) {
+                for (String names : studentList) {
                     writer.println(names);
                 }
             } catch (FileNotFoundException e) {
