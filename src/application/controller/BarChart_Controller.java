946b1d534073d8628154001937c1bbc74407f2b1
Raksha Kagadalu <98128238+Raksha-Kagadalu@users.noreply.github.com>
2023-04-18 17:27:07 -0400

    made few UI changes

--- src/application/controller/BarChart_Controller.java
+++ src/application/controller/BarChart_Controller.java
@@ -13,22 +13,22 @@ public class BarChart_Controller {
     public void setData(int counter1,int  counter2,int counter3, int counter4) {
         XYChart.Series<String, Integer> series1 = new XYChart.Series<>();
         series1.setName("Option1");
-        series1.getData().add(new XYChart.Data<>("Counter", counter1));
+        series1.getData().add(new XYChart.Data<>("Options", counter1));
         chartQuiz.getData().add(series1);
         
         XYChart.Series<String, Integer> series2 = new XYChart.Series<>();
         series2.setName("Option2");
-        series2.getData().add(new XYChart.Data<>("Counter", counter2));
+        series2.getData().add(new XYChart.Data<>("Options", counter2));
         chartQuiz.getData().add(series2);
         
         XYChart.Series<String, Integer> series3 = new XYChart.Series<>();
         series3.setName("Option3");
-        series3.getData().add(new XYChart.Data<>("Counter", counter3));
+        series3.getData().add(new XYChart.Data<>("Options", counter3));
         chartQuiz.getData().add(series3);
         
         XYChart.Series<String, Integer> series4 = new XYChart.Series<>();
         series4.setName("Option4");
-        series4.getData().add(new XYChart.Data<>("Counter", counter4));
+        series4.getData().add(new XYChart.Data<>("Options", counter4));
         chartQuiz.getData().add(series4);
     }
     
